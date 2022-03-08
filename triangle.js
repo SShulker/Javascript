11fc4a919ed29aaeea328a1026397d4acd41e79f
SShulker <tracekosborne@gmail.com>
2022-03-08 03:25:59 -0600

    Create triangle.js

---
+++ triangle.js
@@ -0,0 +1,11 @@
+function triangleArea(base, height) {
+    return base * height / 2;
+}
+
+var area = triangleArea(5, 10);
+var areaAgain = triangleArea(2, 4);
+var areaFinal = triangleArea(4, 20);
+
+console.log(area);
+console.log(areaAgain);
+console.log(areaFinal);
